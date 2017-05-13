ce76a8deb99cc2af6455ed96b06a88d20f41c713
Kevin Dahlberg <nsu_tpt@hotmail.com>
2017-05-13 02:00:34 -0500

    added panel with form to users view

--- client/scripts/controllers/AdminUsersController.js
+++ client/scripts/controllers/AdminUsersController.js
@@ -5,8 +5,41 @@
 * @return
 */
 
-myApp.controller('AdminUsersController', ['AdminService',
-function(AdminService){
+myApp.controller('AdminUsersController', ['AdminService', '$mdPanel',
+function(AdminService, $mdPanel, mdPanelRef){
   var users = this;
+  this._mdPanel = $mdPanel;
+
+
+  users.showDialog = function(el) {
+    var target = el.target;
+    var position = this._mdPanel.newPanelPosition()
+        .absolute()
+        .center();
+
+    var config = {
+      attachTo: angular.element(document.body),
+      templateUrl: 'views/partials/newUserForm.html',
+      panelClass: 'demo-dialog-example',
+      position: position,
+      trapFocus: true,
+      zIndex: 150,
+      clickOutsideToClose: true,
+      escapeToClose: true,
+      focusOnOpen: true
+    };
+
+    this._mdPanel.open(config);
+  };
+
+
+    this._mdPanelRef = mdPanelRef;
+
+
+  users.closeDialog = function() {
+    this._mdPanelRef && this._mdPanelRef.close().then(function() {
+      angular.element(document.querySelector('.demo-dialog-open-button')).focus();
+    });
+  };
 
 }]);
