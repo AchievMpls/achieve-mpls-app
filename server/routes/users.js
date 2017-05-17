a19a10b4d5e9166464c716d1f1c7d355cff29b2c
EmilyH <EmilyHHLam@gmail.com>
2017-05-16 23:15:45 -0500

    setup CUD adminUser

--- server/routes/users.js
+++ server/routes/users.js
@@ -54,13 +54,14 @@ router.delete('/delete/:id', function(req, res) {
 });//end router.delete
 
 
-router.post('/add', function(req, res) {
+router.post('/postUser', function(req, res) {
+  console.log('req body', req.body);
   pool.connect(function(errorConnectingToDb, db, done) {
     if (errorConnectingToDb) {
       res.sendStatus(500);
     } else {
-      db.query('INSERT INTO "users" ("fname", "lname", "email", "role", "password", "session_id") VALUES ($1,$2,$3,$4,$5,$6);',
-      req.body.fname, req.body.lname, req.body.email, req.body.role, req.body.password, req.body.session_id,
+      db.query('INSERT INTO "users" ("fname", "lname", "email", "role", "password", "session_id", "grade") VALUES ($1,$2,$3,$4,$5,$6,$7);',
+      req.body.fname, req.body.lname, req.body.email, req.body.role, req.body.password, req.body.session_id, req.body.grade,
       function(queryError, result) {
         done();
         if (queryError) {
@@ -74,14 +75,14 @@ router.post('/add', function(req, res) {
 });//end router.post
 
 
-router.put('/update', function(req, res) {
+router.put('/updateUser', function(req, res) {
   var id = req.body.id;
   pool.connect(function(errorConnectingToDb, db, done) {
   if (errorConnectingToDb) {
     res.sendStatus(500);
   } else {
-    db.query('UPDATE "users" SET "fname"=$1, "lname"=$2, "email"=$3, "role"=$4, "password"=$5, "session_id"=$6 WHERE "id" = $7;',
-    req.body.fname, req.body.lname, req.body.email, req.body.role, req.body.password, req.body.session_id,
+    db.query('UPDATE "users" SET "fname"=$1, "lname"=$2, "email"=$3, "role"=$4, "password"=$5, "session_id"=$6, "grade"=$7 WHERE "id" = $8;',
+    req.body.fname, req.body.lname, req.body.email, req.body.role, req.body.password, req.body.session_id, req.body.grade,
       function(queryError, result) {
         done();
         if (queryError) {
