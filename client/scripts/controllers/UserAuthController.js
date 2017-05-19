912ef29967832ad5364e8c79bde8a7f9818551db
Teigen Leonard <teigenl@gmail.com>
2017-05-19 08:11:59 -0500

    route params added to userAuthController.js

--- client/scripts/controllers/UserAuthController.js
+++ client/scripts/controllers/UserAuthController.js
@@ -6,6 +6,13 @@
 */
 myApp.controller('UserAuthController', ['AdminService', 'AuthService', '$routeParams',
 function(AdminService, Authservice, $routeParams){
-  // Route params with code
-
+  userAuth = this;
+  // Route params with code grab from route param and pass to put statement which checks with if
+  // statement whether param code is same as db code. ß
+  if($routeParams.code != undefined) {
+  userAuth.code.tempCode = $routeParams.code;
+} else {
+  // Code was saved in the UserService before
+  // bouncing user to log in screen
+}
 }]);
