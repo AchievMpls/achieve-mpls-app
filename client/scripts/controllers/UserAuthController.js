cb90f1170076ea15213f94868f3a80a77a44563e
Kevin Dahlberg <nsu_tpt@hotmail.com>
2017-05-22 15:02:11 -0500

    debugging

--- client/scripts/controllers/UserAuthController.js
+++ client/scripts/controllers/UserAuthController.js
@@ -14,7 +14,6 @@ function(AdminService, AuthService, $routeParams, $http, $location, $mdDialog){
   * @param Object with username which is their email, and password
   * @return
   */
-<<<<<<< HEAD
   user.loginUser = function(user) {
     console.log('login gets here', user);
     if( user.username === '' || user.password === '') {
@@ -44,31 +43,6 @@ function(AdminService, AuthService, $routeParams, $http, $location, $mdDialog){
   };
 
 
-=======
- user.loginUser = function(user) {
-      console.log('login gets here', user);
-        if( user.username === '' || user.password === '') {
-          $mdDialog.show(
-            $mdDialog.alert()
-            .clickOutsideToClose(true)
-            .title('Incomplete Form')
-            .textContent("Please enter a username and password")
-            .ariaLabel('Alert Dialog')
-            .ok('OK')
-          );
-          } else {
-            AuthService.loginUser(user);
-          }
-        };
-
-  user.logout = function (){
-    $http.get('/user/logout').then(function(response){
-      console.log('logout pressed');
-    });
-    $location.path('/login');
-  };
-
->>>>>>> develop
   /**
   * registerAdmin function
   * @desc allows administrator to establish initial account - should only be used once
