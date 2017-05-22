a4321752360904e3143073e765b8f3001cddf4b1
Teigen Leonard <teigenl@gmail.com>
2017-05-22 10:53:19 -0500

    update naming on client.js and UseAuthController to be consistent

--- client/scripts/controllers/UserAuthController.js
+++ client/scripts/controllers/UserAuthController.js
@@ -12,11 +12,11 @@ function(AdminService, AuthService, $routeParams, $http, $location, $mdDialog){
   /**
   * @desc function to login any user
   * @param Object with username which is their email, and password
-  * @return 
+  * @return
   */
  user.loginUser = function(user) {
       console.log('login gets here', user);
-        if( !user.username || !user.password ) {
+        if( user.username === '' || user.password === '') {
           $mdDialog.show(
             $mdDialog.alert()
             .clickOutsideToClose(true)
@@ -30,6 +30,7 @@ function(AdminService, AuthService, $routeParams, $http, $location, $mdDialog){
           }
         };
   /**
+  * registerAdmin function
   * @desc allows administrator to establish initial account - should only be used once
   * @param Object with properties admin.fname, admin.lname, admin.email, admin.password
   * @return
@@ -50,44 +51,44 @@ user.registerAdmin = function(admin) {
          }
        };
   /**
-  * User Auth Controller
+  * addUserPwd
   * @desc user to create the password, if the two inputs are not the same, popup alert is shown
   * @param the user enters in password
   * @return pass the active code and password pass to authService
   */
-    login.addUserPwd = function(user) {
-      var create = user.passwordCreate;
-      var confirm = user.passwordConfirm;
-      //if either the new password and confirm are not filled
-      if (!create || !confirm) {
+  user.addUserPwd = function(user) {
+    var create = user.passwordCreate;
+    var confirm = user.passwordConfirm;
+    //if either the new password and confirm are not filled
+    if (!create || !confirm) {
+      $mdDialog.show(
+        $mdDialog.alert()
+        .clickOutsideToClose(true)
+        .title('Create Password')
+        .textContent('Please fillout both fields.')
+        .ariaLabel('Alert Dialog')
+        .ok('OK!')
+      );
+    } else {
+      if (user.passwordCreate !== user.passwordConfirm) {
         $mdDialog.show(
           $mdDialog.alert()
           .clickOutsideToClose(true)
           .title('Create Password')
-          .textContent('Please fillout both fields.')
+          .textContent('Please make sure the two passwords are the same.')
           .ariaLabel('Alert Dialog')
           .ok('OK!')
         );
       } else {
-        if (user.passwordCreate !== user.passwordConfirm) {
-          $mdDialog.show(
-            $mdDialog.alert()
-            .clickOutsideToClose(true)
-            .title('Create Password')
-            .textContent('Please make sure the two passwords are the same.')
-            .ariaLabel('Alert Dialog')
-            .ok('OK!')
-          );
-        } else {
-          user = {
-            chance_token: $routeParams.code,
-            password: user.passwordConfirm
-          };
-          console.log('hashpwd ', user);
-          AuthService.addUserPwd(user);
-        }
+        user = {
+          chance_token: $routeParams.code,
+          password: user.passwordConfirm
+        };
+        console.log('hashpwd ', user);
+        AuthService.addUserPwd(user);
       }
+    }
 
 
-    };
+  };
 }]);
