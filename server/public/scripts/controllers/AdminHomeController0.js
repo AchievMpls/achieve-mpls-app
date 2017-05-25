490914b2a1fa146d8be0ff405b7925723cce4720
Kevin Dahlberg <nsu_tpt@hotmail.com>
2017-05-25 12:44:02 -0500

    added hamburger for nav bar on smaller screen sizes

---
+++ server/public/scripts/controllers/AdminHomeController0.js
@@ -0,0 +1,12 @@
+/**
+* Admin Home Controller
+* @desc controls the Admin Home View
+* @param AdminService
+* @return
+*/
+
+myApp.controller('AdminHomeController', ['AdminService',
+function(AdminService){
+  var home = this;
+
+}]);
