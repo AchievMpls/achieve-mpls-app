907794ca2aa31f0635f1c0f7f07cd4e3e198787b
Teigen Leonard <teigenl@gmail.com>
2017-05-25 18:08:06 -0500

    all routes working

--- server/routes/users.js
+++ server/routes/users.js
@@ -75,6 +75,27 @@ router.put('/deactivateUser', function(req, res) {
   }
 });//end router.put
 
+router.post('/postUser', function(req, res) {
+  console.log('req body', req.body);
+  var session_id = parseInt(req.body.session_id);
+  pool.connect(function(errorConnectingToDb, db, done) {
+    if (errorConnectingToDb) {
+      res.sendStatus(500);
+    } else {
+      db.query('INSERT INTO "users" ("fname", "lname", "email", "role", "password", "session_id") VALUES ($1,$2,$3,$4,$5,$6);',
+      [req.body.fname, req.body.lname, req.body.email, req.body.role, req.body.password, session_id],
+      function(queryError, result) {
+        done();
+        if (queryError) {
+          res.sendStatus(500);
+        } else {
+          res.sendStatus(201);
+        }
+      });
+    }
+  });
+});//end router.post
+
 router.put('/updateUser', function(req, res) {
   console.log('updating, we have: ', req.body);
   var id = req.body.id;
