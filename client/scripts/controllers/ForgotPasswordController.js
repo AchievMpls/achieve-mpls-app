2e6dacec1e13f2c9bf88d88c6639d4d2adfde9be
Kevin Dahlberg <nsu_tpt@hotmail.com>
2017-09-17 19:54:42 -0500

    forgot pw function written

--- client/scripts/controllers/ForgotPasswordController.js
+++ client/scripts/controllers/ForgotPasswordController.js
@@ -9,6 +9,22 @@
    function(AdminService, AuthService, $mdDialog, $mdPanel) {
      var pw = this;
 
-
+     /**
+      * @function forgotpw
+      * @desc displays confirmation message and sends new pw to user email
+      * @param user email
+      * @return user pw reset, email sent to user with reset pw link
+      */
+     pw.forgotpw = function(email) {
+         $mdDialog.show(
+           $mdDialog.alert()
+           .clickOutsideToClose(true)
+           .title('Reset Password')
+           .textContent("Reset Password link has been sent to the e-mail provided")
+           .ariaLabel('Alert Dialog')
+           .ok('OK')
+         );
+         AuthService.resetpw(email);
+       };
    }
  ]);
